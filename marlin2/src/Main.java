2362761c2cd6efe583e6acabddcd4b29e4e2ef8c
zhenhuiguo1995 <njugzh@gmail.com>
2019-04-23 15:39:10 -0700

    update project

--- marlin2/src/Main.java
+++ marlin2/src/Main.java
@@ -4,10 +4,10 @@ import marlin.graphicsLib.Window;
 
 public class Main {
     public static void main(String[] args){
-//        Window.PANEL = new Squares();
-//        Window.PANEL = new PaintInk();
-//        Window.PANEL = new ShapeTrainer();
-//        Window.PANEL = new SimpleReaction();
+        //Window.PANEL = new Squares();
+        //Window.PANEL = new PaintInk();
+        //Window.PANEL = new ShapeTrainer();
+        Window.PANEL = new SimpleReaction();
         Window.PANEL = new Music2();
         Window.launch();
 
